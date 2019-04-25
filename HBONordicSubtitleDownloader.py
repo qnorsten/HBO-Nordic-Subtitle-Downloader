94737e73300399abddf295c738dfa90b3e46777f
Qnorsten <qnorsten@hotmail.com>
2019-04-25 21:37:56 +0200

    version 0.2.0 HBO now require you to be login to view subtitles
    fixes https://github.com/qnorsten/HBO-Nordic-Subtitle-Downloader/issues/12

--- HBONordicSubtitleDownloader.py
+++ HBONordicSubtitleDownloader.py
@@ -13,12 +13,16 @@ import sys
 import re
 import unicodedata
 import argparse
+import json
+import base64
+import os
+import uuid
 from urllib.parse import urlparse, urljoin
 
 import requests
 from bs4 import BeautifulSoup as Soup
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 class HBONordicSubtitleDownloader():
     def __init__(self):
@@ -26,7 +30,20 @@ class HBONordicSubtitleDownloader():
         self.no_srt = args.no_srt
         self.no_xml = args.no_srt
         self.languages = args.languages
-        
+        self.username = args.username
+        self.password = args.password
+        self.device_token = None
+        self.get_auth_headers()
+
+    def get_auth_headers(self):
+        if os.path.isfile('auth_codes.json'):
+            with open('auth_codes.json', 'rb') as f:
+                data = json.load(f)
+            self.device_id = data.get('device_id',uuid.uuid4())
+            self.device_token = data['device_token']
+        else:
+            self.device_id = uuid.uuid4().urn[9:]
+            self.login()
 
     #originaly copied from svtplay-dl (https://github.com/spaam/svtplay-dl)
     def ensure_unicode(self,s):
@@ -120,10 +137,39 @@ class HBONordicSubtitleDownloader():
        
 
 
-    def getAndSaveSubtitles(self,url=None):
-        ##Main function
 
-        
+    def login(self):
+        encoded_login = '{}:{}'.format(self.username, base64.b64encode(self.password.encode()).decode())
+        encoded_login = base64.b64encode(encoded_login.encode())
+        login_url = 'https://api-hbon.hbo.clearleap.com:443/cloffice/client/device/login?language=sv_hbon'
+        login_headers = {
+            'X-Clearleap-DeviceId': self.device_id,
+            'X-Client-Name': 'web',
+            'X-Client-Version': '3.8.3',
+            'Authorization': 'Basic {}'.format(encoded_login.decode())
+        }
+        payload = '<device><type>web</type><deviceId>{}</deviceId></device>'.format(self.device_id)
+        login = requests.post(login_url, headers=login_headers, data=payload)
+        if login.status_code != 200:
+            raise ValueError('Unable to login, is supplied username and password correct?')
+        soup = Soup(login.content, 'html.parser')
+        self.device_token = soup.find('token').contents[0]
+
+        self.saveFile('auth_codes.json', json.dumps({'device_id': self.device_id, 'device_token': self.device_token}))
+
+    def getAndSaveSubtitles(self,url=None):
+
+        if not self.device_token:
+            self.login()
+
+        ##Main function
+        headers = {}
+
+        headers['X-Clearleap-DeviceToken'] = self.device_token
+        headers['X-Clearleap-DeviceId'] = self.device_id
+        headers['X-Client-Name'] = 'web'
+        headers['X-Client-Version'] = '3.8.3'
+
         #Example Whole series url =https://se.hbonordic.com/cloffice/client/web/browse/5408e6ca-474a-4638-9f35-fe5e67e00737
         #Example season url = "https://api-hbon.hbo.clearleap.com/cloffice/client/web/browse/3bc87b76-737d-4c32-8d1b-8d0257c1e7c8"
         #Example one episode url = "https://se.hbonordic.com/series/gomorrah/season-1/episode-1/1f10ced-005e3fa02dc"
@@ -140,8 +186,11 @@ class HBONordicSubtitleDownloader():
         url = parse.scheme + '://' + parse.netloc
         url = urljoin(url, 'cloffice/client/web/browse/'+ id)
         #get XML with links to subtitles
-        data = requests.get(url).content
-        
+        data = requests.get(url, headers=headers)
+        if data.status_code != 200:
+            self.login()
+            data = requests.get(url, headers=headers)
+        data = data.content
         soup = Soup(data,'html.parser')
 
         #Find each episode
@@ -165,17 +214,23 @@ class HBONordicSubtitleDownloader():
                 else:
                     title = item.find('title').text
                     filename = title
-                for subUrl in subTitleUrls:
-                    subUrl = subUrl.get("href")
+                for sub in subTitleUrls:
+                    subUrl = sub.get("href")
+                    lang = sub.get("lang").replace('_hbon', '')
                     if self.languages:
-                        if self.checkLanguage(subUrl) in self.languages:
-                            self.saveSubtitle(filename,subUrl)
+                        if lang in self.languages:
+                            self.saveSubtitle('{}.{}'.format(filename, lang), subUrl)
                     else:
-                        self.saveSubtitle(filename,subUrl)
+                        self.saveSubtitle('{}.{}'.format(filename, lang), subUrl)
             #if on show page take season urls and call this function again
             else:
-                seasonLink = item.find('link').contents
-                seasonLinks.append(seasonLink[0])
+                try:
+                    if item.find('media:keywords').contents[0] == 'season':
+                        # print(item)
+                        seasonLink = item.find('link').contents
+                        seasonLinks.append(seasonLink[0])
+                except AttributeError:
+                    pass
         if seasonLinks:    
             for seasonLink in seasonLinks:
                 #Prevent loop in case a video is missing subtitles
@@ -188,13 +243,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download subtitles from hbo nordic")
     parser.add_argument("url",help="The url to the movie or series to download subtitles from")
     parser.add_argument("--version", help="Displays the current version",action='version', version=__version__)
+    parser.add_argument("-u, --username", help="email used to login", dest='username')
+    parser.add_argument("-p, --password", help="password", dest='password')
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--no-srt",help="Do not convert downloaded subtitle to srt",action="store_true")
     group.add_argument("--no-xml",help="Do not save the raw xml subtitle",action="store_true")
     parser.add_argument("--languages",help="Specify languages to save in two letter country code as used by HBO Nordic ",nargs='+',choices=['SV','DA','NO','FI'])
     args = parser.parse_args()
-    
+
     HBO = HBONordicSubtitleDownloader()
+
     HBO.getAndSaveSubtitles()
     sys.exit(0)
     
